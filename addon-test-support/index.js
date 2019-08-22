ba962a0423847e7ba77e388d707e1ea4ce7333ba
Max Babich <max22dev@gmail.com>
2019-08-22 16:49:42 +0300

    Update index.js

--- addon-test-support/index.js
+++ addon-test-support/index.js
@@ -5,6 +5,12 @@ export async function upload(selector, file) {
   let input = find(selector);
   assert(`Selector '${selector}' is not input element.`, input.tagName === 'INPUT');
   assert(`File must be instance of File/Blob type`, file instanceof Blob);
-
-  return await triggerEvent(input, 'change', { files: [file] });
+  
+  let ev;
+  try{
+    ev = await triggerEvent(input, 'change', { files: [file] });
+  } catch(e){
+    console.log(e);
+  }
+  return ev;
 }
